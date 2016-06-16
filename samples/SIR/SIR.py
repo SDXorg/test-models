1cc3feacc477ac1d4c70f8788262d1bfe1aaba49
James Houghton <James.p.houghton@gmail.com>
2016-06-16 18:42:45 -0400

    add test on updimensioning

--- samples/SIR/SIR.py
+++ samples/SIR/SIR.py
@@ -1,121 +1,267 @@
 
+"""
+Python model test-models/samples/SIR/SIR.py
+Translated using PySD version 0.5.1
+"""
 from __future__ import division
 import numpy as np
+
+from pysd.functions import cache
 from pysd import functions
+
+_subscript_dict = {}
+
+_namespace = {
+    'Total Population': 'total_population',
+    'Recovering': 'recovering',
+    'Susceptible': 'susceptible',
+    'TIME STEP': 'time_step',
+    'SAVEPER': 'saveper',
+    'Contact Infectivity': 'contact_infectivity',
+    'Infectious': 'infectious',
+    'FINAL TIME': 'final_time',
+    'INITIAL TIME': 'initial_time',
+    'Duration': 'duration',
+    'Succumbing': 'succumbing',
+    'Recovered': 'recovered'}
+
+
+@cache('run')
+def total_population():
+    """
+    Total Population
+    ----------------
+    (total_population)
+    Persons
+
+    This is just a simplification to make it easer to track how many folks
+                there are without having to sum up all the stocks.
+    """
+    return 1000
+
+
+@cache('run')
+def final_time():
+    """
+    FINAL TIME
+    ----------
+    (final_time)
+    Day
+
+    The final time for the simulation.
+    """
+    return 100
+
+
+@cache('step')
+def recovering():
+    """
+    Recovering
+    ----------
+    (recovering)
+    Persons/Day
+
+
+    """
+    return infectious() / duration()
+
+
+@cache('step')
+def susceptible():
+    """
+    Susceptible
+    -----------
+    (susceptible)
+    Persons
+
+    The population that has not yet been infected.
+    """
+    return _state['susceptible']
+
+
+def _init_recovered():
+    """
+    Implicit
+    --------
+    (_init_recovered)
+    See docs for recovered
+
+    Provides initial conditions for recovered function
+    """
+    return 0
+
+
+def _init_infectious():
+    """
+    Implicit
+    --------
+    (_init_infectious)
+    See docs for infectious
+
+    Provides initial conditions for infectious function
+    """
+    return 5
+
+
+@cache('run')
+def contact_infectivity():
+    """
+    Contact Infectivity
+    -------------------
+    (contact_infectivity)
+    Persons/Persons/Day
+
+    A joint parameter listing both how many people you contact, and how likely
+                you are to give them the disease.
+    """
+    return 0.3
+
+
+@cache('step')
+def infectious():
+    """
+    Infectious
+    ----------
+    (infectious)
+    Persons
+
+    The population with the disease, manifesting symptoms, and able to
+                transmit it to other people.
+    """
+    return _state['infectious']
+
+
+@cache('step')
+def _drecovered_dt():
+    """
+    Implicit
+    --------
+    (_drecovered_dt)
+    See docs for recovered
+
+    Provides derivative for recovered function
+    """
+    return recovering()
+
+
+@cache('step')
+def _dinfectious_dt():
+    """
+    Implicit
+    --------
+    (_dinfectious_dt)
+    See docs for infectious
+
+    Provides derivative for infectious function
+    """
+    return succumbing() - recovering()
+
+
+@cache('step')
+def saveper():
+    """
+    SAVEPER
+    -------
+    (saveper)
+    Day [0,?]
+
+    The frequency with which output is stored.
+    """
+    return time_step()
+
+
+@cache('run')
+def initial_time():
+    """
+    INITIAL TIME
+    ------------
+    (initial_time)
+    Day
+
+    The initial time for the simulation.
+    """
+    return 0
+
+
+def _init_susceptible():
+    """
+    Implicit
+    --------
+    (_init_susceptible)
+    See docs for susceptible
+
+    Provides initial conditions for susceptible function
+    """
+    return total_population()
+
+
+@cache('run')
+def duration():
+    """
+    Duration
+    --------
+    (duration)
+    Days
+
+    How long are you infectious for?
+    """
+    return 5
+
+
+@cache('run')
+def time_step():
+    """
+    TIME STEP
+    ---------
+    (time_step)
+    Day [0,?]
+
+    The time step for the simulation.
+    """
+    return 0.03125
+
+
+@cache('step')
+def _dsusceptible_dt():
+    """
+    Implicit
+    --------
+    (_dsusceptible_dt)
+    See docs for susceptible
+
+    Provides derivative for susceptible function
+    """
+    return -succumbing()
+
+
+@cache('step')
+def succumbing():
+    """
+    Succumbing
+    ----------
+    (succumbing)
+    Persons/Day
+
+
+    """
+    return susceptible() * infectious() / total_population() * contact_infectivity()
+
+
+@cache('step')
+def recovered():
+    """
+    Recovered
+    ---------
+    (recovered)
+    Persons
+
+    These people have recovered from the disease. Yay! Nobody dies in this
+                model.
+    """
+    return _state['recovered']
+
 
 def time():
     return _t
-
-def contact_infectivity():
-    """
-    
-    """
-
-    output = 0.3
-	
-
-    return output
-
-def duration():
-    """
-    
-    """
-
-    output = 5
-	
-
-    return output
-
-def infectious():
-    return _state['infectious']
-
-def _infectious_init():
-    return 5
-
-def _dinfectious_dt():
-    return succumbing()-recovering()
-
-def recovered():
-    return _state['recovered']
-
-def _recovered_init():
-    return 0
-
-def _drecovered_dt():
-    return recovering()
-
-def recovering():
-    """
-    
-    """
-
-    output = infectious()/duration()
-
-    return output
-
-def succumbing():
-    """
-    
-    """
-
-    output = susceptible()*infectious()/total_population()* contact_infectivity()
-
-    return output
-
-def susceptible():
-    return _state['susceptible']
-
-def _susceptible_init():
-    return total_population()
-
-def _dsusceptible_dt():
-    return -succumbing()
-
-def total_population():
-    """
-    
-    """
-
-    output = 1000
-	
-
-    return output
-
-def final_time():
-    """
-    
-    """
-
-    output = 100
-	
-
-    return output
-
-def initial_time():
-    """
-    
-    """
-
-    output = 0
-	
-
-    return output
-
-def saveper():
-    """
-    
-    """
-
-    output = time_step()
-
-    return output
-
-def time_step():
-    """
-    
-    """
-
-    output = 0.03125
-	
-
-    return output
+functions.time = time
+functions.initial_time = initial_time
--- tests/number_handling/test_number_handling.py
+++ tests/number_handling/test_number_handling.py
@@ -1,92 +1,146 @@
 
+"""
+Python model test-models/tests/number_handling/test_number_handling.py
+Translated using PySD version 0.5.1
+"""
 from __future__ import division
 import numpy as np
+
+from pysd.functions import cache
 from pysd import functions
+
+_subscript_dict = {}
+
+_namespace = {
+    'INITIAL TIME': 'initial_time',
+    'equality': 'equality',
+    'quotient target': 'quotient_target',
+    'TIME STEP': 'time_step',
+    'denominator': 'denominator',
+    'numerator': 'numerator',
+    'FINAL TIME': 'final_time',
+    'SAVEPER': 'saveper',
+    'quotient': 'quotient'}
+
+
+@cache('step')
+def equality():
+    """
+    equality
+    --------
+    (equality)
+
+
+
+    """
+    return functions.if_then_else(quotient() == quotient_target(), 1, 0)
+
+
+@cache('run')
+def final_time():
+    """
+    FINAL TIME
+    ----------
+    (final_time)
+    Month
+
+    The final time for the simulation.
+    """
+    return 1
+
+
+@cache('run')
+def denominator():
+    """
+    denominator
+    -----------
+    (denominator)
+
+
+
+    """
+    return 4
+
+
+@cache('run')
+def numerator():
+    """
+    numerator
+    ---------
+    (numerator)
+
+
+
+    """
+    return 3
+
+
+@cache('step')
+def saveper():
+    """
+    SAVEPER
+    -------
+    (saveper)
+    Month [0,?]
+
+    The frequency with which output is stored.
+    """
+    return time_step()
+
+
+@cache('run')
+def initial_time():
+    """
+    INITIAL TIME
+    ------------
+    (initial_time)
+    Month
+
+    The initial time for the simulation.
+    """
+    return 0
+
+
+@cache('run')
+def time_step():
+    """
+    TIME STEP
+    ---------
+    (time_step)
+    Month [0,?]
+
+    The time step for the simulation.
+    """
+    return 1
+
+
+@cache('run')
+def quotient_target():
+    """
+    quotient target
+    ---------------
+    (quotient_target)
+
+
+
+    """
+    return 0.75
+
+
+@cache('step')
+def quotient():
+    """
+    quotient
+    --------
+    (quotient)
+
+
+
+    """
+    return numerator() / denominator()
+
 
 def time():
     return _t
-
-# Share the `time` function with the module for `step`, `pulse`, etc.
-functions.__builtins__.update({'time':time})
-
-
-def equality():
-    """
-    
-    """
-    loc_dimension_dir = 0 
-    output = functions.if_then_else(quotient()==quotient_target(), 1,0)
-
-    return output
-
-def denominator():
-    """
-    
-    """
-    loc_dimension_dir = 0 
-    output = 4
-
-    return output
-
-def numerator():
-    """
-    
-    """
-    loc_dimension_dir = 0 
-    output = 3
-
-    return output
-
-def quotient():
-    """
-    
-    """
-    loc_dimension_dir = 0 
-    output = numerator()/denominator()
-
-    return output
-
-def quotient_target():
-    """
-    
-    """
-    loc_dimension_dir = 0 
-    output = 0.75
-
-    return output
-
-def final_time():
-    """
-    
-    """
-    loc_dimension_dir = 0 
-    output = 1
-
-    return output
-
-def initial_time():
-    """
-    
-    """
-    loc_dimension_dir = 0 
-    output = 0
-
-    return output
-
-def saveper():
-    """
-    
-    """
-    loc_dimension_dir = 0 
-    output = time_step()
-
-    return output
-
-def time_step():
-    """
-    
-    """
-    loc_dimension_dir = 0 
-    output = 1
-
-    return output
+functions.time = time
+functions.initial_time = initial_time
