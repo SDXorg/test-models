706d4df2d3f769150788eaf869b5e1d400e25164
Billy Schoenberg <bschoenberg@iseesystems.com>
2017-09-20 14:32:23 -0400

    updated xmile script to export stella results and screenshot (requires 1.4.2) XMUtil is updated with some array support

--- xmile.bash
+++ xmile.bash
@@ -3,7 +3,7 @@
 #ARCHITECT="/Applications/Stella Architect.app"
 #XMUTIL="XMUtil"
 XMUTIL="/Users/bschoenberg/code/personal/xmutil/DerivedData/XMUtil/Build/Products/Debug/XMUtil"
-ARCHITECT="/Users/bschoenberg/code/dev_mac/StellaQt/DerivedData/StellaQt/Build/Products/Debug/Stella Architect.app"
+ARCHITECT="/Users/bschoenberg/code/dev_branch/StellaQt/DerivedData/StellaQt/Build/Products/Debug/Stella Architect.app"
 DIR="./tests"
 
 for input_dir in $DIR
@@ -17,8 +17,11 @@ do
 	    if [ -f "$f.xmile" ]; then
 	    	cp "$f.xmile" "$f.stmx"
 
+	    	CSV_NAME="$(dirname $f)/output_stella.csv"
+	    	PNG_NAME="$(dirname $f)/stella_screenshot.png"
+
 	    	echo "Opening in Architect $f.stmx"
-	    	open -W -a "$ARCHITECT" "$f.stmx" --args -r -s -q
+	    	open -W -a "$ARCHITECT" "$f.stmx" --args -ssm $PWD/$PNG_NAME -r -xall $PWD/$CSV_NAME  -s -q 
 	    else
 	    	echo "XMUtil hit an error"
 	    	sleep 10
